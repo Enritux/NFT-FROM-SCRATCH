89df071b4613a221f2697f4241e0e50ba4fdf9dd
Enrique <enrique.benavides08@gmail.com>
2021-12-19 01:56:00 -0600

    Ok

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -5,10 +5,14 @@ import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract AdvancedCollectible is ERC721, VRFConsumerBase {
 
+    bytes32 internal keyHash;
+    uint256 public fee;
+
     constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash) public
     VRFConsumer(_VRFCoordinator, _LinkToken)
     ERC721("SexyCactus", "SEXC"){
-        
+        keyHash = _keyhash;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK
     }
 
     function createCollectible(uint256 userProvidedSeed, string memory tokenURI)
